ccce6072d05923a4d4ec742518427f09149aed04
Ian Link <ian@linkportfolio.net>
2013-02-21 12:00:07 -0800

    cleanup

--- test.cc
+++ test.cc
@@ -5,59 +5,11 @@
 #include <cassert>
 #include <iostream>
 
-// gets base path then appends material path to the base path
-// gets base path then appends material path to the base path
-char* getMtlPath(const char* path, char* mat){
-  
-  char* p;
-  int size = 0;
-
-  p = strrchr(path, '/');
-  if(p){
-    int start = p-path+1;
-    size += start + 2 + strlen(mat); // the two is for the slash and the final end-of-string \0 character
-    char* mtlPath = (char*) malloc(size);
-    printf("start: %i\n", start);
-    strncpy(mtlPath, path, start);
-    strcat(mtlPath, mat);
-    return mtlPath;
-  } else { // no base path, so we just return the string
-    // allocate so we can always perform "free" later
-    char* mtlPath = (char*) malloc(strlen(mat));
-    return strcpy(mtlPath, mat);
-  }
-}
-
 int
 main(
   int argc,
   char **argv)
 {
-  char* matlPath = getMtlPath("test.obj", "test.mat");
-  // char* matlPath = getMtlPath("rain/test.obj", "test.mat");
-  printf("matpath: %s\n", matlPath);
-  printf("sizeof matpath: %i\n", strlen(matlPath));
-  char path[] = "rain/something/test.obj";
-  // char path[] = "test.obj";
-  char basePath[4096];
-  strcpy (basePath, path);
-
-  char *p;
-  p = strrchr(basePath, '/');
-  if(p){
-    p[0] = '\0';
-  } else {
-    basePath[0] = '\0';
-  }
-  printf("path: %s\n", basePath);
-
-  char mat[] = "mat.mtl";
-  char matPath[4096];
-  strcpy(matPath, basePath);
-  strcat(matPath, "/");
-  strcat(matPath, mat);
-  printf("mat path: %s\n", matPath);
-
   // std::string inputfile = "cornell_box.obj";
   std::string inputfile = "obj/cornell_box.obj";
   std::vector<tinyobj::shape_t> shapes;
@@ -109,8 +61,6 @@ main(
   //   }
   //   printf("\n");
   // }
-
-  free(matlPath);
 
   return 0;
 }
